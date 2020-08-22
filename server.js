56ef07f51ceeff04d066e7930e5103cc3ce47468
adityaitalia <italiaaditya@gmail.com>
2020-08-23 00:56:42 +0530

    upating db host

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image=require('./controllers/image');
 const db=knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-adjacent-58556',
-    user : 'postgres',
-    password : 'aditya888',
-    database : 'smartBrain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
